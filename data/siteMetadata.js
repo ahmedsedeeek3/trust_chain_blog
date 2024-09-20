ddab2f6da1b52ee50f4ef83b9be082f6b673c76a
ahmedsedeeek3 <79186904+ahmedsedeeek3@users.noreply.github.com>
2024-09-20 13:59:00 +0200

    init

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,17 +1,18 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Trustchain Blog',
+  author: 'Trustchain lab',
+  headerTitle: 'Trustchain',
+  description: 'A blog created by Trustchain lab',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
+  siteRepo: '-',
+  siteLogo: `/static/images/logo.png`,
+  siteIcon: `/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
+  email: 'info@trustchain.info',
   github: 'https://github.com',
   x: 'https://twitter.com/x',
   // twitter: 'https://twitter.com/Twitter',
@@ -22,7 +23,7 @@ const siteMetadata = {
   instagram: 'https://www.instagram.com',
   locale: 'en-US',
   // set to true if you want a navbar fixed to the top
-  stickyNav: false,
+  stickyNav: true,
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
